5ed87dc16548f85cbe64990ce77915173c7c399f
AHBICJ <810988732@qq.com>
2019-05-09 22:09:06 +0800

    new first commit

---
+++ scripts/slide.js
@@ -0,0 +1,10 @@
+const Slide = [...document.querySelectorAll('.topSlide')];
+let currentDisplay = 0;
+Slide[currentDisplay].style.opacity=1;
+const totalImg = 4;
+setInterval(changeSlide,4500);
+function changeSlide(){
+    Slide[currentDisplay].style.opacity=0;
+    currentDisplay = (currentDisplay+1) % totalImg;
+    Slide[currentDisplay].style.opacity=1;
+}
