66bd423d6608053d6564f913e770999969244432
AHBICJ <810988732@qq.com>
2019-05-11 00:15:45 +0800

    add slide js

--- scripts/slide.js
+++ scripts/slide.js
@@ -2,7 +2,7 @@ const Slide = [...document.querySelectorAll('.topSlide')];
 let currentDisplay = 0;
 Slide[currentDisplay].style.opacity=1;
 const totalImg = 4;
-setInterval(changeSlide,4500);
+setInterval(changeSlide,4000);
 function changeSlide(){
     Slide[currentDisplay].style.opacity=0;
     currentDisplay = (currentDisplay+1) % totalImg;
