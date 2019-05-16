f47c4c616e564982fee2089f3c99497faf8e6d91
AHBICJ <810988732@qq.com>
2019-05-16 18:03:01 +0800

    shisai

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,8 +1,8 @@
 const Slide = [...document.querySelectorAll('.topSlide')];
 let currentDisplay = 0;
 Slide[currentDisplay].style.opacity=1;
-const totalImg = 4;
-setInterval(changeSlide,4000);
+const totalImg = 2;
+setInterval(changeSlide,6000);
 function changeSlide(){
     Slide[currentDisplay].style.opacity=0;
     currentDisplay = (currentDisplay+1) % totalImg;
